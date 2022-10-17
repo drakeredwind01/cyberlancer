3b9c9cf5f9f400fbff092db10019cf97f38fa9cd
drakeredwind01 <drakeredwind01@gmail.com>
2022-10-17 13:16:53 -0700

    restarting with another tut

--- cyberlancer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Gig, Purchase, Review
-
-admin.site.register(Profile)
-admin.site.register(Gig)
-admin.site.register(Purchase)
-admin.site.register(Review)

4fe29373e88f22013da101e817e97bb52b9d5f87
drakeredwind01 <43461626+drakeredwind01@users.noreply.github.com>
2022-10-20 10:58:20 -0700

    bye old cyberlancer

--- cyberlancer/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Gig
-
-class GigForm(ModelForm):
-    class Meta:
-        model = Gig
-        fields = ['title', 'category', 'description', 'price', 'photo', 'status']

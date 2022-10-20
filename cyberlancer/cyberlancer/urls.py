1155e605d6198d04466ce0dd81853096124e5ffe
drakeredwind01 <drakeredwind01@gmail.com>
2022-10-20 10:38:01 -0700

    really going somewhere
        -[Done] create virtual env
        -[Done] Install and setup
        -[Done] Init database, super user, test run
        -[    ] Create core app for base pages
        -[    ] Creat app for userprofiles
        -[    ] Create app for products
         [    ]    -Add categorty model
        -[    ] List out newest products on the front page
        -[    ] Product detail view
        -[    ] categorty view
        -[    ] Simple search
        -[    ] Vendor detail page
        -[    ] Make it possible to sign up, log in and log out
        -[    ] Create my account page
        -[    ] Add product to the categorty
        -[    ] View cart
        -[    ] Checkout
        -[    ] Implement payment gateway
        -[    ] Admin pages for the Vendor
        -[    ] Deploy project

--- cyberlancer/cyberlancer/urls.py
+++ cyberlancer/cyberlancer/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from core.views import frontpage, about
+
+
+
 urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ]

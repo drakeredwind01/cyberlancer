475cf5518d07af29c824119ee0d4e73698f638c6
drakeredwind01 <drakeredwind01@gmail.com>
2022-10-22 11:19:32 -0700

    added requirments simple_deploy

--- cyberlancer/cyberlancer/settings.py
+++ cyberlancer/cyberlancer/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'simple_deploy',
 ]
 
 MIDDLEWARE = [
--- cyberlancer/cyberlancer/wsgi.py
+++ cyberlancer/cyberlancer/wsgi.py
@@ -14,3 +14,23 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cyberlancer.settings')
 
 application = get_wsgi_application()
+
+
+"""
+import os 
+import sys 
+
+## assuming your django settings file is at '/home/drakeredwind01/cyberlancer'
+## and your manage.py is at 
+path= '/home/drakeredwind01/cyberlancer'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cyberlancer.settings'
+
+## then, for django >=1.5:
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+## or, for older django <= 1.4
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler
+"""
